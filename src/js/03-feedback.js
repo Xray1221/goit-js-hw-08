99349a1b8a80f1e09b8b03871a2f943acba86b64
Xray1221 <105057432+Xray1221@users.noreply.github.com>
2022-09-29 14:35:19 +0300

    completed tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const inputRefs = {
+    email: document.querySelector('[name="email"]'),
+    message:  document.querySelector('[name="message"]')
+}
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onSubmit);
+
+let localState = {
+    email: '',
+    message: ''
+}
+
+let localStorageState = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (localStorageState) {
+    localState = {...localState, ...localStorageState}
+}
+
+updateInputs();
+
+function onInput(event) {
+    localState[event.target.name] = event.target.value;
+    updateLocalStorage();
+}
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(localState);
+    localState.email = '';
+    localState.message = '';
+    updateLocalStorage();
+    updateInputs();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(localState)); 
+}
+
+function updateInputs() {
+    inputRefs.email.value = localState.email;
+    inputRefs.message.value = localState.message;
+}
